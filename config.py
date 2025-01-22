9e95ba5bb6aa55e1c02d714061ade51709091812
live9807 <k76734182@gmail.com>
2025-01-22 09:09:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from pyrogram import filters
 import os
 
 class Config:
-    API_ID = "YOUR API ID"
-    API_HASH = "YOUR API HASH"  
+    API_ID = "20137303"
+    API_HASH = "d1a8a231897f62d26a8cf9974b1303f8"  
     TOKEN = os.environ.get("TOKEN", "YOUR BOT TOKEN")
-    MONGO_URL = "YOUR MANGO DB URL"
+    MONGO_URL = "mongodb+srv://sevexedits:5JyQbk44RuB2jQj6@cluster0.lezcv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     START_PIC = "https://files.catbox.moe/z2ajhe.jpg"
     SUDOERS = filters.user(["YOUR OWNER ID"])
