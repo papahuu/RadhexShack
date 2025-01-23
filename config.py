01f2dda2a87335ca9e0ed1da3bb21f8e724388cf
Rishu <vishwakarmarishabh366@gmail.com>
2025-01-23 21:33:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ class Config:
     API_HASH = "d1a8a231897f62d26a8cf9974b1303f8"  
     TOKEN = os.environ.get("TOKEN", "YOUR BOT TOKEN")
     MONGO_URL = "mongodb+srv://sevexedits:5JyQbk44RuB2jQj6@cluster0.lezcv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    START_PIC = "https://files.catbox.moe/z2ajhe.jpg"
+    START_PIC = "https://files.catbox.moe/na1a5r.jpg"
     SUDOERS = filters.user(["YOUR OWNER ID"])
