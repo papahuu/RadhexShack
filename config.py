e8b81417204d8b31d3c4c6515fda183f42c5d3cc
live9807 <k76734182@gmail.com>
2025-01-26 08:33:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config:
     API_ID = "20137303"
     API_HASH = "d1a8a231897f62d26a8cf9974b1303f8"  
     TOKEN = os.environ.get("TOKEN", "YOUR BOT TOKEN")
-    MONGO_URL = "mongodb+srv://sevexedits:5JyQbk44RuB2jQj6@cluster0.lezcv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    MONGO_URL = "mongodb+srv://rishustringhack:shivi@stringhack.vt5r7.mongodb.net/?retryWrites=true&w=majority&appName=stringhack"
     START_PIC = "https://files.catbox.moe/na1a5r.jpg"
     SUDOERS = filters.user(["YOUR OWNER ID"])
