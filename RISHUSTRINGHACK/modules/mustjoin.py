487d05354593366e14bc59836bb51dab05907c7a
TheRadhe <rajveer202415@gmail.com>
2025-02-02 23:54:05 +0530

    Update mustjoin.py

--- RISHUSTRINGHACK/modules/mustjoin.py
+++ RISHUSTRINGHACK/modules/mustjoin.py
@@ -5,7 +5,7 @@ from RISHUSTRINGHACK import app
 
 #--------------------------
 
-MUST_JOIN = "nenobots"
+MUST_JOIN = "ll_BOTCHAMBER_ll"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
@@ -31,7 +31,7 @@ async def must_join_channel(app: Client, msg: Message):
                             ],
                             [
                                 InlineKeyboardButton("๏Jᴏɪɴ๏", url="https://t.me/ll_BOTCHAMBER_ll"),
-                                InlineKeyboardButton("๏Jᴏɪɴ๏", url="https://t.me/ur_rishu_143")
+                                InlineKeyboardButton("๏Jᴏɪɴ๏", url="https://t.me/DP_WORLD7")
                             ]
                         ]
                     )
